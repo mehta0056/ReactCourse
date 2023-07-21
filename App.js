624dfef1f851a820f111e71fe83fd1356d5fc5a1
Naresh <naresh.mehta0056@gmail.com>
2023-07-21 09:45:22 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("div",{id:'parenet'},React.createElement("div",{id:'child'},React.createElement('h1',{},"Hi, I am Heading Tag")))
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
